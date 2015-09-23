269bc550510e624b7e8d6ff4a7501f134223e9b2
Danny <dani_geme15@hotmail.com>
2015-09-23 16:38:34 -0500

    1.-Extension boolean Method for a string that check if a string is a palindrome

--- src/Palindrome.cs
+++ src/Palindrome.cs
@@ -4,6 +4,19 @@ namespace CSharp.Training
 {
     public class Palindrome
     {
-       
+        public bool pal(string P)
+        {
+            if (P.isPalindrome())
+            {
+                Console.WriteLine(P + " is palindrome");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine(P + " is NOT palindrome");
+                return false;
+            }
+
+        }
     }
 }
